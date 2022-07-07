1fe584d08227ef72cdb2c1799ebc8b9ee745bf23
hung.vm.dev <hungvumanh2001@gmail.com>
2022-07-07 22:11:42 +0700

    done repositories bai qlbanhang

---
+++ src/main/java/com/example/demo_spring/repository/BillDetailRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo_spring.repository;
+
+import com.example.demo_spring.model.BillDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillDetailRepository extends JpaRepository<BillDetail, Long> {
+}
